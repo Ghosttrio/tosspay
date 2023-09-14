60d3e8b2861ef8c34326355d776b60c089359f83
Ghosttrio <ghosttrio@naver.com>
2023-09-14 17:13:56 +0900

    0914

--- src/main/java/com/pay/paydomain/PayRepository.java
+++ src/main/java/com/pay/paydomain/PayRepository.java
@@ -3,6 +3,9 @@ package com.pay.paydomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PayRepository extends JpaRepository<Information, Long> {
+
 }
--- src/main/java/com/pay/paydomain/PayService.java
+++ src/main/java/com/pay/paydomain/PayService.java
@@ -1,7 +1,14 @@
 package com.pay.paydomain;
 
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -18,4 +25,30 @@ public class PayService {
                 .build();
         payRepository.save(information);
     }
+
+    @Transactional
+    public ResponseEntity<String> requestFinalPayment(String paymentKey, String orderId, int amount) {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        String key = "test_sk_P9BRQmyarYG51vMnDLX3J07KzLNk:";
+
+        String encode = new String(Base64.getEncoder().encode(key.getBytes(StandardCharsets.UTF_8)));
+
+        headers.setBasicAuth(encode);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        JSONObject json = new JSONObject();
+        json.put("orderId", orderId);
+        json.put("paymentKey", paymentKey);
+        json.put("amount", amount);
+
+        ResponseEntity<String> response = rest.postForEntity(
+                "https://api.tosspayments.com/v1/payments/confirm",
+                new HttpEntity<>(json, headers),
+                String.class
+        );
+        return response;
+    }
+
 }
